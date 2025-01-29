ff004b8e4409f0ec7a5619385052fae3e4bb1ab7
Nitesh Shah <niteshshah000@gmail.com>
2025-01-29 16:10:15 +0530

    //

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -191,7 +191,7 @@ class AuthController extends Controller
 
         if ($existingUser) {
             $otp = $this->sendOtp($request->number); 
-            return response()->json(['message' => 'User already exists, OTP sent!', 'otp' => $otp], 200);
+            return response()->json(['message' => 'User already exists, OTP sent!','status' => 200], 200);
         }
 
         $otp = $this->sendOtp($request->number); 

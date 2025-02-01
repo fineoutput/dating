16de66a4562e92a57825b1acae5aae8ea34244be
Nitesh Shah <niteshshah000@gmail.com>
2025-02-01 19:12:54 +0530

    //

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -189,10 +189,13 @@ class AuthController extends Controller
     if ($request->status === 'insert' && $request->number) {
         $existingUser = User::where('number', $request->number)->first();
 
-        if($existingUser->status == 0){
-            return response()->json(['message' => 'You have been blocked by Admin, You cannot log in yet', 'status' => false], 200);
-        }
+        
         if ($existingUser) {
+            
+            if($existingUser->status == 0){
+                return response()->json(['message' => 'You have been blocked by Admin, You cannot log in yet', 'status' => false], 200);
+            }
+
             $otp = $this->sendOtp($request->number); 
             return response()->json(['message' => 'User already exists, OTP sent!','status' => true], 200);
         }

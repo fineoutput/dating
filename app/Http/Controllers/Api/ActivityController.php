1cf823345f72d7d292bcca4318d8175cc7937fa2
Nitesh Shah <niteshshah000@gmail.com>
2025-10-07 16:57:28 +0530

    //

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -3448,6 +3448,7 @@ public function friendcount_one(Request $request)
 
         $chat = Chat::where('sender_id', $user->id)
                     ->where('receiver_id', $userItem->id)
+                    ->where('send_type', 'group')
                     ->orderBy('id', 'DESC')
                     ->first();
 

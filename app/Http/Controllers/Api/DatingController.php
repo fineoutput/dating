9d4d213840abd2c134de342abf860893e630f3f4
Nitesh Shah <niteshshah000@gmail.com>
2025-10-14 15:35:16 +0530

    //

--- app/Http/Controllers/Api/DatingController.php
+++ app/Http/Controllers/Api/DatingController.php
@@ -2021,7 +2021,7 @@ public function acceptslide(Request $request)
         $cupid->status = 2;
 
         $firebaseService = new FirebaseService();
-        $title = $request->title ?? "{$authUser->name} Accept Your Like";
+        $title = $request->title ?? "{$authUser->name} Accept Your Like Request";
 
         if (!$sendnotification->fcm_token) {
             Log::warning("No FCM token for user ID: {$sendnotification->id}, rendom: {$sendnotification->rendom}");

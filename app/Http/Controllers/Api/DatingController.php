1be52997d07ee1e082cc84764b8b4b2622134c98
Nitesh Shah <niteshshah000@gmail.com>
2025-11-03 18:04:25 +0530

    //

--- app/Http/Controllers/Api/DatingController.php
+++ app/Http/Controllers/Api/DatingController.php
@@ -828,7 +828,7 @@ public function MatchingUsersdetailes(Request $request)
             }
         }
 
-        $remainingSwipes = max($allowedInterest - $usedSwipes, 0);
+        $remainingSwipes = max((int)$allowedInterest - (int)$usedSwipes, 0);
 
     return response()->json([
         'message' => 'Matching users found successfully',
@@ -2315,7 +2315,7 @@ public function handleUserInteractions(Request $request)
                 ->orWhere('super_like', 1);
             })
             ->count();
-            
+
             // $usedSwipes = SlideLike::where('matched_user', $user->id)
             //     ->where('liked_user', 1)
             //     ->whereBetween('created_at', [$activeSubscription->activated_at, $activeSubscription->expires_at])

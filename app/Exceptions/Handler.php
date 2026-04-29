dfd5baf724db2ae399c24a40e191a5a5fa7a8645
Nitesh Shah <niteshshah000@gmail.com>
2026-04-29 16:05:46 +0530

    //

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -38,4 +39,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json([
+            'status' => 401,
+            'message' => 'Unauthorized.'
+        ], 401);
+    }
+
 }
